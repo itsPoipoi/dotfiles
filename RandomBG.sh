b14c74aab0fdaf11ecc7f2e51255fae5ea446829
itsPoipoi <poipoigit@gmail.com>
2025-12-28 05:30:36 +0100

    ...

--- RandomBG.sh
+++ RandomBG.sh
@@ -4,25 +4,22 @@
 WP_FOLDER=~/Pictures/Wallpapers
 
 # Time in seconds to change wallpaper
-sleep 5
+sleep 1200
 WAIT_TIME=1200
 STARTPID=$(pidof swaybg)
 kill "$STARTPID"
 
-FILE2START=$(find "$WP_FOLDER" -type f -name '*' | shuf -n1)
-swaybg -o DP-1 -i "$FILE2START" -m fill &
-
 while true; do
-    PID=$(pidof swaybg | awk '{print $2}')
-    FILE=$(find "$WP_FOLDER" -type f -name '*' | shuf -n1)
-    swaybg -o eDP-1 -o DP-3 -i "$FILE" -m fill &
-    sleep 0.5
-    kill "$PID"
-    sleep "$WAIT_TIME"
-    PID=$(pidof swaybg | awk '{print $2}')
-    FILE=$(find "$WP_FOLDER" -type f -name '*' | shuf -n1)
-    swaybg -o eDP-1 -o DP-1 -i "$FILE" -m fill &
-    sleep 0.5
-    kill "$PID"
-    sleep "$WAIT_TIME"
+  PID=$(pidof swaybg | awk '{print $2}')
+  FILE=$(find "$WP_FOLDER" -type f -name '*' | shuf -n1)
+  swaybg -o eDP-1 -o DP-3 -i "$FILE" -m fill &
+  sleep 0.5
+  kill "$PID"
+  sleep "$WAIT_TIME"
+  PID=$(pidof swaybg | awk '{print $2}')
+  FILE=$(find "$WP_FOLDER" -type f -name '*' | shuf -n1)
+  swaybg -o eDP-1 -o DP-1 -i "$FILE" -m fill &
+  sleep 0.5
+  kill "$PID"
+  sleep "$WAIT_TIME"
 done
--- ThemeSetup.sh
+++ ThemeSetup.sh
@@ -5,27 +5,27 @@
 \rm -rf $HOME/.local/share/omarchy/themes/gruvbox/
 \rm -rf $HOME/.local/share/omarchy/themes/nord/
 \rm -rf $HOME/.local/share/omarchy/themes/rose-pine/
+\rm -rf $HOME/.local/share/omarchy/themes/kanagawa/
+\rm -rf $HOME/.local/share/omarchy/themes/hackerman/
 \rm -f $HOME/.config/omarchy/themes/catppuccin-latte
 \rm -f $HOME/.config/omarchy/themes/everforest
 \rm -f $HOME/.config/omarchy/themes/flexoki-light
 \rm -f $HOME/.config/omarchy/themes/gruvbox
 \rm -f $HOME/.config/omarchy/themes/nord
 \rm -f $HOME/.config/omarchy/themes/rose-pine
+\rm -f $HOME/.config/omarchy/themes/kanagawa
+\rm -f $HOME/.config/omarchy/themes/hackerman
 omarchy-theme-install https://github.com/JJDizz1L/aetheria.git
 omarchy-theme-install https://github.com/bjarneo/omarchy-aura-theme
-omarchy-theme-install https://github.com/Hydradevx/omarchy-azure-glow-theme
 omarchy-theme-install https://github.com/HANCORE-linux/omarchy-blackturq-theme.git
-omarchy-theme-install https://github.com/hoblin/omarchy-cobalt2-theme
 omarchy-theme-install https://github.com/HANCORE-linux/omarchy-demon-theme.git
 omarchy-theme-install https://github.com/ShehabShaef/omarchy-drac-theme
 omarchy-theme-install https://github.com/catlee/omarchy-dracula-theme
 omarchy-theme-install https://github.com/dotsilva/omarchy-dotrb-theme
 omarchy-theme-install https://github.com/bjarneo/omarchy-futurism-theme
-omarchy-theme-install https://github.com/steve-lohmeyer/omarchy-mars-theme
 omarchy-theme-install https://github.com/monoooki/omarchy-neo-sploosh-theme
 omarchy-theme-install https://github.com/sc0ttman/omarchy-one-dark-pro-theme
 omarchy-theme-install https://github.com/bjarneo/omarchy-pulsar-theme
-omarchy-theme-install https://github.com/Grey-007/purple-moon
 omarchy-theme-install https://github.com/HANCORE-linux/omarchy-sapphire-theme.git
 omarchy-theme-install https://github.com/motorsss/omarchy-solarizedosaka-theme
 omarchy-theme-install https://github.com/tahayvr/omarchy-sunset-drive-theme
