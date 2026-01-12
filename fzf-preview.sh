2c72184d36e4e9b674deec2dad3f48ffca81ac91
itsPoipoi <poipoigit@gmail.com>
2026-01-12 01:26:14 +0100

    ...

--- fzf-preview.sh
+++ fzf-preview.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
-#
-# The purpose of this script is to demonstrate how to preview a file or an
-# image in the preview window of fzf.
-#
-# Dependencies:
-# - https://github.com/sharkdp/bat
-# - https://github.com/hpjansson/chafa
-# - https://iterm2.com/utilities/imgcat
 
 if [[ $# -ne 1 ]]; then
   >&2 echo "usage: $0 FILENAME[:LINENO][:IGNORED]"
@@ -52,22 +44,11 @@ dim=${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}
 if [[ $dim == x ]]; then
   dim=$(stty size </dev/tty | awk '{print $2 "x" $1}')
 elif ! [[ $KITTY_WINDOW_ID ]] && ((FZF_PREVIEW_TOP + FZF_PREVIEW_LINES == $(stty size </dev/tty | awk '{print $1}'))); then
-  # Avoid scrolling issue when the Sixel image touches the bottom of the screen
-  # * https://github.com/junegunn/fzf/issues/2544
   dim=${FZF_PREVIEW_COLUMNS}x$((FZF_PREVIEW_LINES - 1))
 fi
 
-# Use icat (from Kitty) if kitten is installed
 if [[ $KITTY_WINDOW_ID ]] || [[ $GHOSTTY_RESOURCES_DIR ]] && command -v kitten >/dev/null; then
-  # 1. 'memory' is the fastest option but if you want the image to be scrollable,
-  #    you have to use 'stream'.
-  #
-  # 2. The last line of the output is the ANSI reset code without newline.
-  #    This confuses fzf and makes it render scroll offset indicator.
-  #    So we remove the last line and append the reset code to its previous line.
   kitten icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place="$dim@0x0" "$file" | sed '$d' | sed $'$s/$/\e[m/'
-
-#  Cannot find any suitable method to preview the image
 else
   file "$file"
 fi
