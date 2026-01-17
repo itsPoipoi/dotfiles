fc753dd8e6b360d2173b714fd0748a2012875649
itsPoipoi <poipoigit@gmail.com>
2026-01-17 22:01:23 +0100

    Disabled blur, tweaked opacities

--- hypr/.config/hypr/ChangeBlur.sh
+++ hypr/.config/hypr/ChangeBlur.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 # Script for changing blurs on the fly
 
-STATE=$(hyprctl -j getoption decoration:blur:passes | jq ".int")
+STATE=$(hyprctl -j getoption decoration:blur:enabled | jq ".int")
 
-if [ "${STATE}" == "1" ]; then
+if [ "${STATE}" == "0" ]; then
+  hyprctl keyword decoration:blur:enabled 1
   hyprctl keyword decoration:blur:size 2
   hyprctl keyword decoration:blur:passes 2
-  hyprctl keyword decoration:blur:brightness 0.25
+  hyprctl keyword decoration:blur:brightness 0.35
 else
+  hyprctl keyword decoration:blur:enabled 0
   hyprctl keyword decoration:blur:size 1
   hyprctl keyword decoration:blur:passes 1
   hyprctl keyword decoration:blur:brightness 0.7
