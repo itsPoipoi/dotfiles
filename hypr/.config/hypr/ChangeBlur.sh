32d0798c0c2c85bb73f25545cf99c361a854e857
itsPoipoi <poipoigit@gmail.com>
2026-01-17 15:22:40 +0100

    More kitty transparency, less focus mode brightness

--- hypr/.config/hypr/ChangeBlur.sh
+++ hypr/.config/hypr/ChangeBlur.sh
@@ -4,11 +4,11 @@
 STATE=$(hyprctl -j getoption decoration:blur:passes | jq ".int")
 
 if [ "${STATE}" == "1" ]; then
-	hyprctl keyword decoration:blur:size 2
-	hyprctl keyword decoration:blur:passes 2
-	hyprctl keyword decoration:blur:brightness 0.4
+  hyprctl keyword decoration:blur:size 2
+  hyprctl keyword decoration:blur:passes 2
+  hyprctl keyword decoration:blur:brightness 0.25
 else
-	hyprctl keyword decoration:blur:size 1
-	hyprctl keyword decoration:blur:passes 1
-	hyprctl keyword decoration:blur:brightness 0.7
+  hyprctl keyword decoration:blur:size 1
+  hyprctl keyword decoration:blur:passes 1
+  hyprctl keyword decoration:blur:brightness 0.7
 fi
