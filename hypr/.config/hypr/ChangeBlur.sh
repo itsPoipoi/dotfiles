1a96689d865803ff860b0e0da3662a6f8462196c
itsPoipoi <poipoigit@gmail.com>
2026-01-21 18:36:25 +0100

    More opacity tweaks

--- hypr/.config/hypr/ChangeBlur.sh
+++ hypr/.config/hypr/ChangeBlur.sh
@@ -5,9 +5,9 @@ STATE=$(hyprctl -j getoption decoration:blur:enabled | jq ".int")
 
 if [ "${STATE}" == "0" ]; then
   hyprctl keyword decoration:blur:enabled 1
-  hyprctl keyword decoration:blur:size 2
-  hyprctl keyword decoration:blur:passes 2
-  hyprctl keyword decoration:blur:brightness 0.35
+  hyprctl keyword decoration:blur:size 3
+  hyprctl keyword decoration:blur:passes 3
+  hyprctl keyword decoration:blur:brightness 0.25
 else
   hyprctl keyword decoration:blur:enabled 0
   hyprctl keyword decoration:blur:size 1
