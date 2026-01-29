d19b98d68d13c3bed11ed15ab3b127d10170db9a
itsPoipoi <poipoigit@gmail.com>
2026-01-29 15:49:45 +0100

    Change 2nd monitor background on startup

--- RandomBG.sh
+++ RandomBG.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 
-# Wallpaper directory
 WP_FOLDER=~/Pictures/Wallpapers
+WALLPAPERS=$(find "$WP_FOLDER" -type f -name '*')
+CURRENT_BACKGROUND_LINK="$HOME/.config/omarchy/current/background"
+
+# Change 2nd monitor background on startup
+sleep 3
+FILE=$(echo "$WALLPAPERS" | shuf -n1)
+swaybg -o DP-1 -i "$FILE" -m fill &
 
 # Time in seconds to change wallpaper
 WAIT_TIME=1200
 sleep "$WAIT_TIME"
-
-# Cache wallpaper list for efficiency
-WALLPAPERS=$(find "$WP_FOLDER" -type f -name '*')
-CURRENT_BACKGROUND_LINK="$HOME/.config/omarchy/current/background"
 
 # Add signal handling for graceful shutdown
 trap 'echo "Terminating..."; killall swaybg 2>/dev/null; exit' INT TERM
