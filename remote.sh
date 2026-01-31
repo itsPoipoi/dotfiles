3b239bb94017c2f5c808f329dbd9b22a95dc09f7
itsPoipoi <poipoigit@gmail.com>
2026-01-31 19:15:22 +0100

    Tweak sleep behavior

--- remote.sh
+++ remote.sh
@@ -2,14 +2,12 @@
 
 BLUE=$'\e[0;34m'
 GREEN=$'\e[0;32m'
-
 if [[ ! -d "$HOME/dotfiles" ]]; then
   echo "${BLUE}Cloning dotfiles repository...${GREEN}"
   git clone https://github.com/itsPoipoi/dotfiles.git "$HOME"/dotfiles
 else
   echo "${BLUE}Updating dotfiles repository...${GREEN}"
   git -C "$HOME"/dotfiles pull
+  sleep 1
 fi
-
-sleep 0.5
 bash "$HOME"/dotfiles/setup.sh
