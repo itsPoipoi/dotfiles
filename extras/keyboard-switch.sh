c68ad170479eae6ab8b1f74259c14505fdd5029a
itsPoipoi <poipoigit@gmail.com>
2026-01-31 02:42:15 +0100

    Script: toggle azerty & kanata

---
+++ extras/keyboard-switch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+if systemctl --user is-active kanata.service; then
+  systemctl --user stop kanata.service
+  hyprctl switchxkblayout all 1
+  notify-send "⌨ Azerty"
+else
+  systemctl --user start kanata.service
+  hyprctl switchxkblayout all 0
+  notify-send "⌨ Ergo-L"
+fi
