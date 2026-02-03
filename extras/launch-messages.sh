b35275c4867012a432834b1d98ca8200e4d901ce
itsPoipoi <poipoigit@gmail.com>
2026-02-03 21:17:33 +0100

    Fix script quirks

--- extras/launch-messages.sh
+++ extras/launch-messages.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 omarchy-launch-or-focus-webapp "Messenger" "https://www.messenger.com" &>/dev/null &
+sleep 0.5
 omarchy-launch-or-focus-webapp "Instagram" "https://www.instagram.com/direct/inbox/" &>/dev/null &
+sleep 0.5
 omarchy-launch-or-focus-webapp "WhatsApp" "https://web.whatsapp.com/" &>/dev/null &
-omarchy-launch-or-focus-webapp "Messages" "https://messages.google.com/web/conversations" &>/dev/null &
-sleep 1
-omarchy-launch-or-focus-webapp "Messages" "https://messages.google.com/web/conversations" &>/dev/null &
+sleep 0.5
+omarchy-launch-or-focus-webapp "Google.Messages" "https://messages.google.com/web/conversations" &>/dev/null &
