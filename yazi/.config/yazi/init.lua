a67c1e0d54e6826e88ed02d6187452a934c58c44
itsPoipoi <poipoigit@gmail.com>
2026-02-25 18:03:19 +0100

    Add augment-command to yazi

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -3,10 +3,27 @@ require("git"):setup({
 	order = 1500,
 })
 
--- ~/.config/yazi/init.lua
 require("relative-motions"):setup({
 	show_numbers = "relative_absolute", -- relative_absolute or none
 	show_motion = true,
 	only_motions = false,
 	enter_mode = "cache_or_first",
+})
+
+-- https://github.com/hankertrix/augment-command.yazi?tab=readme-ov-file#configuration
+th.create_title = { "Create and open:", "Create (dir):" }
+require("augment-command"):setup({
+	prompt = false,
+	default_item_group_for_prompt = "hovered",
+	smart_enter = false,
+	smart_paste = true,
+	skip_single_subdirectory_on_enter = true,
+	skip_single_subdirectory_on_leave = true,
+	use_default_create_behaviour = true,
+	create_item_delay = 0.25,
+	enter_archives = false,
+	extract_retries = 3,
+	recursively_extract_archives = true,
+	reveal_created_archive = true,
+	must_have_hovered_item = false,
 })
