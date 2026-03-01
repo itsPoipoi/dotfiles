f57dff731bd3217fc05f0e04c8a310d894881c46
itsPoipoi <poipoigit@gmail.com>
2026-03-01 20:56:34 +0100

    Improve layout notification

--- extras/toggle-layout.sh
+++ extras/toggle-layout.sh
@@ -2,9 +2,9 @@
 if hyprctl devices | grep "Ergo"; then
   systemctl --user stop kanata.service
   hyprctl switchxkblayout all 1
-  notify-send "💻   Azerty"
+  notify-send "💻   Layout:  AZERTY   |   Kanata:  OFF"
 else
   systemctl --user start kanata.service
   hyprctl switchxkblayout all 0
-  notify-send "💻   Ergo-L"
+  notify-send "💻   Layout:  ERGO-L   |   Kanata:  ON"
 fi
