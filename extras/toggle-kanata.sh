132a8dcd7a60e926a2ab0244ad62ff980e78282a
itsPoipoi <poipoigit@gmail.com>
2026-03-01 20:58:16 +0100

    Switch layout if not Ergo-L

--- extras/toggle-kanata.sh
+++ extras/toggle-kanata.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-if systemctl --user is-active kanata.service; then
-  systemctl --user stop kanata.service
-  notify-send "💻   Stopping Kanata"
+if hyprctl devices | grep "Ergo"; then
+  if systemctl --user is-active kanata.service; then
+    systemctl --user stop kanata.service
+    notify-send "💻   Kanata:  OFF"
+  else
+    systemctl --user start kanata.service
+    notify-send "💻   Kanata:  ON"
+  fi
 else
   systemctl --user start kanata.service
-  notify-send "💻   Starting Kanata"
+  hyprctl switchxkblayout all 0
+  notify-send "💻   Layout:  ERGO-L   |   Kanata:  ON"
 fi
