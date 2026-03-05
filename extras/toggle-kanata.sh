28d74e0739ed5908f70ffe4cb9d65b69ae7feae0
itsPoipoi <poipoigit@gmail.com>
2026-03-05 19:16:36 +0100

    Added notification timeout

--- extras/toggle-kanata.sh
+++ extras/toggle-kanata.sh
@@ -2,13 +2,13 @@
 if hyprctl devices | grep "Ergo‑L"; then
   if systemctl --user is-active kanata.service; then
     systemctl --user stop kanata.service
-    notify-send "💻   Kanata:  OFF"
+    notify-send -t 1000 "💻   Kanata:  OFF"
   else
     systemctl --user start kanata.service
-    notify-send "💻   Kanata:  ON"
+    notify-send -t 1000 "💻   Kanata:  ON"
   fi
 else
   systemctl --user start kanata.service
   hyprctl switchxkblayout all 0
-  notify-send "💻   Layout:  ERGO-L   |   Kanata:  ON"
+  notify-send -t 1000 "💻   Layout:  ERGO-L   |   Kanata:  ON"
 fi
--- extras/toggle-layout.sh
+++ extras/toggle-layout.sh
@@ -2,9 +2,9 @@
 if hyprctl devices | grep "Ergo‑L"; then
   systemctl --user stop kanata.service
   hyprctl switchxkblayout all 1
-  notify-send "💻   Layout:  AZERTY   |   Kanata:  OFF"
+  notify-send -t 1000 "💻   Layout:  AZERTY   |   Kanata:  OFF"
 else
   systemctl --user start kanata.service
   hyprctl switchxkblayout all 0
-  notify-send "💻   Layout:  ERGO-L   |   Kanata:  ON"
+  notify-send -t 1000 "💻   Layout:  ERGO-L   |   Kanata:  ON"
 fi
