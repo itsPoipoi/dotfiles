b088d0ba96ba7240799ee075c56fb2cf2bf4cd7f
itsPoipoi <poipoigit@gmail.com>
2026-05-06 11:12:33 +0200

    Initial Satty config

--- stow.sh
+++ stow.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 if [[ -z $STOW_FOLDERS ]]; then
-    STOW_FOLDERS="fastfetch,fd,hypr,imv,kitty,lazygit,opencode,thunar,yazi,xfce4,zshrc"
+    STOW_FOLDERS="fastfetch,fd,hypr,imv,kitty,lazygit,opencode,satty,thunar,yazi,xfce4,zshrc"
 fi
 
 if [[ -z $DOTFILES ]]; then
